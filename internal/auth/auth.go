0d86fd71dc9acc0b9190abe89ae49ca1c4fa8f45
JustBecerra <justj.becerra@gmail.com>
2026-01-22 11:43:29 -0300

    fix code so ci passes

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,12 +12,12 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "asd", ErrNoAuthHeaderIncluded
+		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "asd123", errors.New("malformed authorization header")
+		return "", errors.New("malformed authorization header")
 	}
 
-	return splitAuth[1], errors.New("break code error")
+	return splitAuth[1], nil
 }
