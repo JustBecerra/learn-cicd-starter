770210b95cb3b3e2e3abbe71cb37f27886218796
Justo Becerra <justj.becerra@gmail.com>
2026-01-30 16:23:47 -0300

    Revert "add another job"

--- json.go
+++ json.go
@@ -30,9 +30,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_, err = w.Write(dat)
-	if err != nil {
-		log.Printf("Error writing JSON: %s", err)
-		return
-	}
+	w.Write(dat)
 }
